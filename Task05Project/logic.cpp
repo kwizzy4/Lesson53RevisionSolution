d60a98dd29cedaf1fdb73c175cba4d891e1f8441
kogicko240 <dmn120112@gmail.com>
2026-04-06 17:23:17 +0300

    17:24

--- Task05Project/logic.cpp
+++ Task05Project/logic.cpp
@@ -3,3 +3,15 @@
 // 
 // Необходимо спроектировать и реализовать программу, 
 // которая подсчитывает количество цифр в заданном целом числе.
+
+#include "logic.h"
+
+int count(int number) {
+	if (number < 0) {
+		return count(-number);
+	}
+	if (number < 10) {
+		return 1;
+	}
+	return 1 + count(number / 10);
+}
